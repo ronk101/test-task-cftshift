9ea2e2dc3cb567f8dbb4f7a4b5b3f2b76759e97a
skpntsv <n.skopintsev@g.nsu.ru>
2023-08-25 20:23:02 +0700

    add some checking

--- sort-it/src/main/java/ru/cft/shift/skopintsev/sort/FileMerger.java
+++ sort-it/src/main/java/ru/cft/shift/skopintsev/sort/FileMerger.java
@@ -28,32 +28,61 @@ public class FileMerger {
                 mergeMultipleFiles(writer);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Не удалось создать выходной файл" + e);
         }
     }
 
-    private void copySingleFile(String inputFile, BufferedWriter writer) throws IOException {
+    private void copySingleFile(String inputFile, BufferedWriter writer) {
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 writer.write(line);
                 writer.newLine();
             }
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось открыть файл" + inputFile + e);
         }
     }
 
-    private void mergeMultipleFiles(BufferedWriter writer) throws IOException {
+    private void mergeMultipleFiles(BufferedWriter writer) {
         while (sortedFiles.size() > 1) {
+
             String firstFile = sortedFiles.poll();
+            if (!fileExists(firstFile)) {
+                System.err.println("Файл '" + firstFile + "не существует. Пропуск");
+                continue;
+            }
+            if (!isFileSorted(firstFile)) {
+                System.err.println("Файл " + firstFile + " неправильно отсортирован. Пропуск");
+                continue;
+            }
+
             String secondFile = sortedFiles.poll();
+            if (!fileExists(secondFile)) {
+                System.err.println("Файл '" + secondFile + "не существует. Пропуск");
+                sortedFiles.addFirst(firstFile);
+                continue;
+            }
+            if (!isFileSorted(secondFile)) {
+                System.err.println("Файл " + secondFile + " неправильно отсортирован. Пропуск");
+                sortedFiles.addFirst(firstFile);
+                continue;
+            }
+
             String mergedFileName = mergeTwoFiles(firstFile, secondFile);
-            sortedFiles.add(0, mergedFileName);
+            if (mergedFileName != null) {
+                sortedFiles.addFirst(mergedFileName);
+            }
         }
 
         copySingleFile(sortedFiles.getFirst(), writer);
     }
 
-    private String mergeTwoFiles(String inputFile1, String inputFile2) throws IOException {
+    private boolean fileExists(String fileName) {
+        return new File(fileName).exists();
+    }
+
+    private String mergeTwoFiles(String inputFile1, String inputFile2) {
         String mergedFileName = createTempFileName();
 
         try (BufferedWriter mergedWriter = new BufferedWriter(new FileWriter(mergedFileName));
@@ -64,15 +93,12 @@ public class FileMerger {
             String line2 = reader2.readLine();
 
             while (line1 != null && line2 != null) {
-                boolean shouldWriteLine1;
-
-                if ((dataType == DataType.NUMERIC && isNumeric(line1) && isNumeric(line2)) ||
-                        (dataType == DataType.STRING)) {
+                try {
                     int comparison = dataType == DataType.NUMERIC ?
                             Integer.compare(Integer.parseInt(line1), Integer.parseInt(line2)) :
                             line1.compareTo(line2);
 
-                    shouldWriteLine1 = (sortingMode == SortingMode.ASCENDING && comparison <= 0) ||
+                    boolean shouldWriteLine1 = (sortingMode == SortingMode.ASCENDING && comparison <= 0) ||
                             (sortingMode == SortingMode.DESCENDING && comparison >= 0);
 
                     if (shouldWriteLine1) {
@@ -82,12 +108,24 @@ public class FileMerger {
                         writeLine(mergedWriter, line2);
                         line2 = reader2.readLine();
                     }
-                } else {
-                    System.out.println("Ошибка: Что-то не так с типом данных");
-                    System.exit(1);
+                } catch (NumberFormatException e) {
+                    // Пропускаем некорректные данные, переходим к следующим строкам
+                    if (dataType == DataType.NUMERIC) {
+                        line1 = reader1.readLine();
+                        line2 = reader2.readLine();
+                    } else if (dataType == DataType.STRING) {
+                        if (line1.compareTo(line2) <= 0) {
+                            writeLine(mergedWriter, line1);
+                            line1 = reader1.readLine();
+                        } else {
+                            writeLine(mergedWriter, line2);
+                            line2 = reader2.readLine();
+                        }
+                    }
                 }
             }
 
+            // Пропускаем оставшиеся строки из файлов
             while (line1 != null) {
                 writeLine(mergedWriter, line1);
                 line1 = reader1.readLine();
@@ -97,29 +135,44 @@ public class FileMerger {
                 writeLine(mergedWriter, line2);
                 line2 = reader2.readLine();
             }
+        } catch (IOException e) {
+            System.err.println("Ошибка при обработке файлов: " + e.getMessage());
+            return null; // Возвращаем null, чтобы обозначить ошибку
         }
 
         return mergedFileName;
     }
 
-    private boolean isNumeric(String str) {
+    private String createTempFileName() {
+        File tempFile;
         try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
+            tempFile = File.createTempFile("merged", ".tmp");
+            tempFile.deleteOnExit();
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось создать временный файл");
         }
-    }
-
-
-    private String createTempFileName() throws IOException {
-        File tempFile = File.createTempFile("merged", ".tmp");
-        tempFile.deleteOnExit();
         return tempFile.getAbsolutePath();
     }
 
     private void writeLine(BufferedWriter writer, String line) throws IOException {
         writer.write(line);
         writer.newLine();
+    }
+
+    private boolean isFileSorted(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String prevLine = null;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (prevLine != null && line.compareTo(prevLine) < 0) {
+                    return false; // Файл не отсортирован
+                }
+                prevLine = line;
+            }
+            return true; // Файл отсортирован
+        } catch (IOException e) {
+            System.err.println("Ошибка при проверке файла: " + e.getMessage());
+            return false;
+        }
     }
 }
