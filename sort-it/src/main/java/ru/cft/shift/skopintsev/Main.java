0e4e8fe44e02f4dea7bc42c8e12d7f8a4ad0940e
skpntsv <n.skopintsev@g.nsu.ru>
2023-08-25 23:25:21 +0700

    update main

--- sort-it/src/main/java/ru/cft/shift/skopintsev/Main.java
+++ sort-it/src/main/java/ru/cft/shift/skopintsev/Main.java
@@ -2,28 +2,14 @@ package ru.cft.shift.skopintsev;
 
 import ru.cft.shift.skopintsev.sort.FileMerger;
 import ru.cft.shift.skopintsev.utils.CommandLineParser;
-import ru.cft.shift.skopintsev.utils.DataType;
-import ru.cft.shift.skopintsev.utils.SortingMode;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         CommandLineParser parser = new CommandLineParser(args);
 
-        SortingMode sortingMode = parser.getSortingMode();
-        DataType dataType = parser.getDataType();
-        String outputFile = parser.getOutputFile();
-        List<String> inputFiles = parser.getInputFiles();
+        FileMerger fileMerger = new FileMerger(parser.getInputFiles(), parser.getOutputFile(),
+                parser.getDataType(), parser.getSortingMode());
 
-        System.out.println(sortingMode);
-        System.out.println(dataType);
-        System.out.println(outputFile);
-        for (String inputFile: inputFiles) {
-            System.out.println(inputFile);
-        }
-
-        FileMerger fileMerger = new FileMerger(inputFiles, outputFile, dataType, sortingMode);
         fileMerger.mergeSortedFiles();
     }
 }
